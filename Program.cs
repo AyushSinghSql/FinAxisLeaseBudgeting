8195951146235395f7f927eacb8b8992fa699e59
AyushSinghSql <ayush.singh@revolvespl.com>
2026-06-25 13:14:49 -0400

    updated the program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+
+
 // using FinAxisLeaseBudgeting.Data;
 // using FinAxisLeaseBudgeting.Interfaces;
 // using FinAxisLeaseBudgeting.Models;
@@ -10,42 +12,43 @@
 // builder.Services.AddDbContext<FinAxisDbContext>(options =>
 //     options.UseNpgsql(connectionString));
 
-// // Add services to the container.
+// // 1. ADD THIS LINE: Configure CORS to allow your local environment and Swagger
+// builder.Services.AddCors(options =>
+// {
+//     options.AddDefaultPolicy(policy =>
+//     {
+//         policy.AllowAnyOrigin()
+//               .AllowAnyMethod()
+//               .AllowAnyHeader();
+//     });
+// });
 
 // builder.Services.AddControllers();
-
-
-// //Register the repository dependency
 
 // builder.Services.AddScoped<ICommLeaseRepository, CommLeaseRepository>();
 // builder.Services.AddScoped<ICommContactRepository, CommContactRepository>();
 // builder.Services.AddScoped<ICommCustomerRepository, CommCustomerRepository>();
 // builder.Services.AddScoped<ICommLeaseUnitRepository, CommLeaseUnitRepository>();
 
-
-// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
 // builder.Services.Configure<PowerBISettings>(builder.Configuration.GetSection("PowerBI"));
 
 // var app = builder.Build();
 
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi(); // <-- Must be here
-//     app.UseSwaggerUI(options =>
-//     {
-//         options.SwaggerEndpoint("/openapi/v1.json", "FinAxis API v1");
-//     });
-// }
+// // 2. ADD THIS LINE: Enable CORS middleware right after app builds
+// app.UseCors();
 
-// app.UseHttpsRedirection();
+// app.MapOpenApi(); 
+// app.UseSwaggerUI(options =>
+// {
+//     options.SwaggerEndpoint("/openapi/v1.json", "FinAxis API v1");
+//     options.RoutePrefix = "swagger";
+// });
 
 // app.UseAuthorization();
-
 // app.MapControllers();
-
 // app.Run();
+
 
 
 using FinAxisLeaseBudgeting.Data;
@@ -60,7 +63,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<FinAxisDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// 1. ADD THIS LINE: Configure CORS to allow your local environment and Swagger
+// Configure CORS
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -78,12 +81,25 @@ builder.Services.AddScoped<ICommContactRepository, CommContactRepository>();
 builder.Services.AddScoped<ICommCustomerRepository, CommCustomerRepository>();
 builder.Services.AddScoped<ICommLeaseUnitRepository, CommLeaseUnitRepository>();
 
-builder.Services.AddOpenApi();
+// --- UPDATE THIS BLOCK: Force HTTPS for OpenAPI/Swagger Requests ---
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        // This forces the "Try it out" button to execute over HTTPS on Render
+        document.Servers = new List<Microsoft.OpenApi.Models.OpenApiServer>
+        {
+            new Microsoft.OpenApi.Models.OpenApiServer { Url = "https://finaxisleasebudgeting.onrender.com" }
+        };
+        return Task.CompletedTask;
+    });
+});
+// -------------------------------------------------------------------
+
 builder.Services.Configure<PowerBISettings>(builder.Configuration.GetSection("PowerBI"));
 
 var app = builder.Build();
 
-// 2. ADD THIS LINE: Enable CORS middleware right after app builds
 app.UseCors();
 
 app.MapOpenApi(); 
